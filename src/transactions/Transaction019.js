6542ed644149892dbaf14a57261a471bd2971894
jv88899 <jv88899@gmail.com>
2020-11-09 12:59:28 -0600

    created 019, added to router

--- src/transactions/Transaction019.js
+++ src/transactions/Transaction019.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import TransactionList from "../TransactionList";
 
-const Transaction018 = ({ companyName }) => (
+const Transaction019 = ({ companyName }) => (
   <>
     <TransactionList activeStep={`007`} />
     <div className="board-wrapper">
@@ -144,17 +144,8 @@ const Transaction018 = ({ companyName }) => (
                   <h5>Liabilities</h5>
                   <ul className="balance-sheet-liabilities">
                     <li>
-                      <Link
-                        to="/019"
-                        style={{
-                          color: "inherit",
-                          cursor: "default",
-                          textDecoration: "none"
-                        }}
-                      >
-                        <span className="item-name blue">Claims Payable</span>
-                        <span className="item-total">18</span>
-                      </Link>
+                      <span className="item-name blue">Claims Payable</span>
+                      <span className="item-total">1</span>
                     </li>
                     <li>
                       <span className="item-name blue">IBNR</span>
@@ -255,4 +246,4 @@ const Transaction018 = ({ companyName }) => (
   </>
 );
 
-export default Transaction018;
+export default Transaction019;
