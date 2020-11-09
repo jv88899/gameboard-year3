03474d097af59089bdb5afc14dfadab36dace745
jv88899 <jv88899@gmail.com>
2020-11-09 09:50:06 -0600

    added click handler to 013

--- src/transactions/Transaction013.js
+++ src/transactions/Transaction013.js
@@ -144,8 +144,17 @@ const Transaction013 = ({ companyName }) => (
                   <h5>Liabilities</h5>
                   <ul className="balance-sheet-liabilities">
                     <li>
-                      <span className="item-name blue">Claims Payable</span>
-                      <span className="item-total">15</span>
+                      <Link
+                        to="/014"
+                        style={{
+                          color: "inherit",
+                          cursor: "default",
+                          textDecoration: "none"
+                        }}
+                      >
+                        <span className="item-name blue">Claims Payable</span>
+                        <span className="item-total">15</span>
+                      </Link>
                     </li>
                     <li>
                       <span className="item-name blue">IBNR</span>
