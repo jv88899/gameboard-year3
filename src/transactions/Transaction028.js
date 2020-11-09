51afed00ad79fb84645c3bb51e61be6789e9bea4
jv88899 <jv88899@gmail.com>
2020-11-09 14:52:59 -0600

    added click handler to 028

--- src/transactions/Transaction028.js
+++ src/transactions/Transaction028.js
@@ -118,8 +118,17 @@ const Transaction028 = ({ companyName }) => (
                   <h5>Assets</h5>
                   <ul className="balance-sheet-assets">
                     <li>
-                      <span className="item-name green">Cash</span>
-                      <span className="item-total">6</span>
+                      <Link
+                        to="/029"
+                        style={{
+                          color: "inherit",
+                          cursor: "default",
+                          textDecoration: "none"
+                        }}
+                      >
+                        <span className="item-name green">Cash</span>
+                        <span className="item-total">6</span>
+                      </Link>
                     </li>
                     <li>
                       <span className="item-name blue">Investments</span>
