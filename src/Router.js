dadd173fa8cfa1b38726c825e450c64b7b9cd836
jv88899 <jv88899@gmail.com>
2020-11-10 10:08:19 -0600

    created 042, added to router

--- src/Router.js
+++ src/Router.js
@@ -41,6 +41,7 @@ import Transaction038 from "./transactions/Transaction038";
 import Transaction039 from "./transactions/Transaction039";
 import Transaction040 from "./transactions/Transaction040";
 import Transaction041 from "./transactions/Transaction041";
+import Transaction042 from "./transactions/Transaction042";
 
 const Router = () => {
   const companyName = "Medica";
@@ -172,6 +173,9 @@ const Router = () => {
         </Route>
         <Route path="/041" exact>
           <Transaction041 companyName={companyName} />
+        </Route>
+        <Route path="/042" exact>
+          <Transaction042 companyName={companyName} />
         </Route>
       </Switch>
     </BrowserRouter>
