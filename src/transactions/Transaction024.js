a5e4985a0ff1f5539f75f6e60bc4f9d27e958ae9
jv88899 <jv88899@gmail.com>
2020-12-09 12:57:27 -0600

    added click handler to 024

--- src/transactions/Transaction024.js
+++ src/transactions/Transaction024.js
@@ -76,8 +76,17 @@ const Transaction024 = ({ companyName }) => (
                 <span className="item-total">18</span>
               </li>
               <li>
-                <span className="item-name red">Administrative Expense</span>
-                <span className="item-total">0</span>
+                <Link
+                  to="/025"
+                  style={{
+                    color: "inherit",
+                    cursor: "default",
+                    textDecoration: "none"
+                  }}
+                >
+                  <span className="item-name red">Administrative Expense</span>
+                  <span className="item-total">0</span>
+                </Link>
               </li>
               <li>
                 <span className="item-name red">Broker Expense</span>
