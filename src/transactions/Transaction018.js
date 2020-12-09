0572fc1609fe456c11289ce8b8299f0519209966
jv88899 <jv88899@gmail.com>
2020-12-09 12:26:06 -0600

    added click handler to 018

--- src/transactions/Transaction018.js
+++ src/transactions/Transaction018.js
@@ -202,8 +202,17 @@ const Transaction018 = ({ companyName }) => (
               <span className="item-total">3</span>
             </li>
             <li>
-              <span className="item-name red">Medical Providers</span>
-              <span className="item-total">1</span>
+              <Link
+                to="/019"
+                style={{
+                  color: "inherit",
+                  cursor: "default",
+                  textDecoration: "none"
+                }}
+              >
+                <span className="item-name red">Medical Providers</span>
+                <span className="item-total">1</span>
+              </Link>
             </li>
             <li>
               <span className="item-name red">Employees</span>
